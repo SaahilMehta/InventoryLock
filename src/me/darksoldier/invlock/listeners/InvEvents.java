e23428eaab2b16df2bd4238576d5434429af7958
Cyanoure <kozelka.r@gmail.com>
2020-05-27 17:06:09 +0200

    Fix some problems

--- src/me/darksoldier/invlock/listeners/InvEvents.java
+++ src/me/darksoldier/invlock/listeners/InvEvents.java
@@ -3,9 +3,9 @@ package me.darksoldier.invlock.listeners;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 
@@ -15,22 +15,11 @@ public class InvEvents implements Listener{
 	
 
 	FileConfiguration config = Main.plugin.getConfig();
-	
-	@EventHandler
-	public void dragEvent(InventoryDragEvent e) {
-		Player p = (Player)e.getWhoClicked();
-		if(!hasBypassPermission(p) && playerInWorld(p)) {
-			e.setCancelled(true);
-			p.updateInventory();
-		}
-		
-		
-	}
-	
+
 	@EventHandler
 	public void clickEvent(InventoryClickEvent e) {
 		Player p = (Player)e.getWhoClicked();
-		if(!hasBypassPermission(p) && playerInWorld(p)) {
+		if(!hasBypassPermission(p) && playerInWorld(p) && slotIsInList(e.getSlot())) {
 			e.setCancelled(true);
 			p.updateInventory();
 		}
@@ -40,7 +29,7 @@ public class InvEvents implements Listener{
 	@EventHandler
 	public void dropEvent(PlayerDropItemEvent e) {
 		Player p = e.getPlayer();
-		if(p.getInventory().getHeldItemSlot() == 4) {
+		if(!hasBypassPermission(p) && playerInWorld(p) && slotIsInList(p.getInventory().getHeldItemSlot())) {
 //		if(!hasBypassPermission(p) && playerInWorld(p)) {
 			e.setCancelled(true);
 			p.updateInventory();
@@ -50,7 +39,7 @@ public class InvEvents implements Listener{
 	@EventHandler
 	public void swapEvent(PlayerSwapHandItemsEvent e) {
 		Player p = e.getPlayer();
-		if(!hasBypassPermission(p) && playerInWorld(p)) {
+		if(!hasBypassPermission(p) && playerInWorld(p) && slotIsInList(p.getInventory().getHeldItemSlot())) {
 			e.setCancelled(true);
 			p.updateInventory();
 		}
@@ -68,6 +57,8 @@ public class InvEvents implements Listener{
 		return config.getStringList("enabled-worlds").contains(p.getWorld().getName());
 	}
 	
-	
+	private boolean slotIsInList(int slotIndex){
+		return config.getStringList("enabled-slots").size() == 0 || config.getIntegerList("enabled-slots").contains(slotIndex+1);
+	}
 
 }
